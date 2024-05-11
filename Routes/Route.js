2846e588a122ba5d95017fec1896cefc7d3d0c15
Kenza <kenzabaccar6@gmail.com>
2024-05-11 12:33:32 +0200

    update file routes

--- Routes/Route.js
+++ Routes/Route.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const IssuesController = require('../Controllers/IssuesController');
 const ProjectsController = require('../Controllers/ProjectsController');
 const SprintsController = require('../Controllers/SprintsController');
-const UserController = require('../Controllers/UserController');
+const UserController = require('../Controllers/UserAzureController');
+router.post('/user', UserController.AddUser);
+router.post('/user/role',UserController.AddRole );
+router.post('/user/assign-role',UserController.AddUserToGroup );
+router.get('/user',UserController.GetAllUsers );
+router.get('/role',UserController.GetAllGroups );
+router.get('/role/members/:GroupId',UserController.GetMembersGroup);
+router.delete('/user/:userId', UserController.DeleteUser);
+router.delete('/role/:GroupId', UserController.DeleteGroup);
+router.patch('/user/:id', UserController.UpdateUser);
 router.get('/issues', IssuesController.getIssues);
-router.post('/user/create',UserController.createUserController);
-router.get('/user', UserController.getAllUsersController);
-router.get('/user/:userID', UserController.getUserByIdController);
-router.delete('/user/:userID', UserController.deleteUserByIdController);
-router.put('/user/:userID', UserController.updateUserByIdController )
 router.get('/projects', ProjectsController.getProjects);
 router.get('/sprints', SprintsController.getAllSprints);
 module.exports = router;
+
